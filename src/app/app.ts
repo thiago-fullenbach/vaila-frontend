69877c27652acf6907799351a7aa7085a1893fc1
Thiago Carvalho Füllenbach <tcfullenbach@gmail.com>
2025-09-27 20:45:07 -0300

    feature/url: Adiciona biblioteca Angular Material (com componentes padrões), tailwind CSS e fontsawesome. Ajusta HTML do componente app, utilizando bibliotecas adicionadas e ajustando views.
    Signed-off-by: Thiago Carvalho Füllenbach <tcfullenbach@gmail.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,27 @@
+import { faArrowUpRightFromSquare, faCopy, faHeart, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Component, inject, signal } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'vaila-root',
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatPaginatorModule,
+    FontAwesomeModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -24,8 +42,8 @@ export class App {
     { hash: "3", shortUrl: "https://www.vai.la.com.br/3", originalUrl: "http://www.qrstuvwx.com.br/d172hdwiud781he" }
   ];
 
-  accessUrl(url: string) {
-    window.open(url, '_blank', 'noopener,noreferrer');
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faArrowUpRightFromSquare, faCopy, faTrash, faHeart);
   }
 
   onSubmit() {
