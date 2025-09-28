248a76a308ec512cbb8265c15b142706a02750af
Thiago Carvalho Füllenbach <tcfullenbach@gmail.com>
2025-09-28 17:22:22 -0300

    feature/url: Altera componente de cadastro de URLs, adicionando lógica para cadastro de URLs e ajustando frontend para exibir a URL cadastrada.
    - Altera template HTML do componente UrlFormCard, adicionando seção de resultado do cadastro e spinner de carregamento.
    - Altera UrlFormCard, implementando função onSubmit. Adiciona UrlService como dependência.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true })
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(
+      withFetch()
+    )
   ]
 };
--- src/app/feature/url/components/url-form-card/url-form-card.ts
+++ src/app/feature/url/components/url-form-card/url-form-card.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 import { UrlFormModel } from '../../model/url-form-model';
+import { UrlDTO, UrlService } from '../../service/url-service';
+import { UrlServiceImpl } from '../../service/url-service-impl';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'vaila-url-form-card',
@@ -11,22 +19,46 @@ import { UrlFormModel } from '../../model/url-form-model';
     MatCardModule,
     MatInputModule,
     MatButtonModule,
+    MatProgressSpinnerModule,
     ReactiveFormsModule,
+    FontAwesomeModule
   ],
   templateUrl: './url-form-card.html',
 })
 export class UrlFormCard {
+  readonly snackbarDuration = 5000;
   readonly urlPattern = /^(https?:\/\/)([\w-]+(\.[\w-]+)+)(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$/;
+
+  private urlService: UrlService = inject(UrlServiceImpl);
+  private snackbar: MatSnackBar = inject(MatSnackBar);
+
+  outputUrl = signal<string>('');
+  isLoading = signal<boolean>(false);
+
   urlForm: FormGroup<UrlFormModel>;
 
-  constructor(fb: FormBuilder) {
+  constructor(fb: FormBuilder, library: FaIconLibrary) {
     this.urlForm = fb.group<UrlFormModel>({
       url: fb.control('', { nonNullable:true, validators: [Validators.required, Validators.pattern(this.urlPattern)] })
     })
+    library.addIcons(faArrowUpRightFromSquare);
   }
 
   onSubmit() {
-    // TODO: Implement
-    console.log(this.urlForm.value.url)
+    let url: string = this.urlForm.value.url!;
+    this.isLoading.update(c => !c);
+    this.urlService.createUrl(url).subscribe({
+      next: (urlDTO: UrlDTO) => {
+        this.isLoading.update(c => !c);
+        this.outputUrl.update(() => urlDTO.shortUrl)
+        this.snackbar.open("URL encurtada", '', {
+          duration: this.snackbarDuration
+        })
+      },
+      error: (err) => {
+        this.isLoading.update(c => !c);
+        console.error(`Erro ao salvar URL: ${err}`);
+      }
+    });
   }
 }
